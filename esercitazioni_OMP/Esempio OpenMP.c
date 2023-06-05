bf971dde17b914ecf569031fe9555a0b4678b7fa
Nickbarbieri1 <barbieri.nic01@gmail.com>
2023-06-05 22:40:31 +0200

    Aggiunta commento al programma

--- esercitazioni_OMP/Esempio OpenMP.c
+++ esercitazioni_OMP/Esempio OpenMP.c
@@ -5,6 +5,7 @@ void ex2() {
 	int id;
 	int base = 5;
 	int slaves = 0;
+	//direttiva che specifica la porzione di codice parallelo e che indica anche i vari tipi delle variabili passate alla funzione parallela generata da OMP
 	#pragma omp parallel shared (slaves) firstprivate(base) private (id)
 	{
 		id = omp_get_thread_num();
